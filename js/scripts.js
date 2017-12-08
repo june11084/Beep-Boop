5d1e4bfd356b46e355cf471dc2ebb5e9860b4c0b
Jun Li <june11084@gmail.com>
2017-12-08 12:35:49 -0800

    have readme, if statements, for loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-var toBeepBoop = function(int) {
+var toBeepBoop = function(int,string) {
   var range = [];
+  var name = string;
   for(var i = 0; i<=int; i++ ) {
-    range[i]=i.toString();
+    if ((i != 0) && (i % 3 == 0)){
+      range[i] = "I'm sorry " + name + " I'm afraid I can't do that.";
+    }
+    for (index = 0; index <= range[i].toString().length; index++)
+
+
+
   }
 
   return range;
+  return name;
 
 };
 
@@ -14,10 +22,11 @@ var toBeepBoop = function(int) {
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+    var name = $("#nameInput").val();
     var number = parseInt($("#numberInput").val());
 
 
-    var numberArray = toBeepBoop(number);
+    var numberArray = toBeepBoop(number,name);
     $("#result").text(numberArray);
 
   });
